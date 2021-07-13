a6a8fec9571136779e8f87456664981a0d607ce1
溪桥 <756445638@qq.com>
2021-07-13 09:49:43 +0800

    remove server error text func

---
+++ svr_callback.go
@@ -0,0 +1,10 @@
+package snap7go
+
+//#cgo CFLAGS: -I .
+//#include "snap7.h"
+//#include <stdlib.h>
+/*
+	// go implemetation prototype
+	extern void S7API GlobalCliAsCallback(void *usrPtr, int opCode, int opResult);
+
+*/
