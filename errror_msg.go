b3968bdf05c7222d6b09ebd3eccef0255ebf4ef7
溪桥 <756445638@qq.com>
2021-07-13 11:45:05 +0800

    wrong of error message

--- errror_msg.go
+++ errror_msg.go
@@ -18,11 +18,7 @@ func Cli_ErrorText(code int) error {
 	var buf [length]byte
 	var errCode = C.Cli_ErrorText(C.int(code), (*C.char)(unsafe.Pointer(&buf[0])), length)
 	if errCode != 0 {
-		if e, ok := cliErrorsTable[int(errCode)]; ok {
-			return e
-		} else {
-			return fmt.Errorf("unknown error code %d", errCode)
-		}
+		return fmt.Errorf(" C.Cli_ErrorText failed code :%d,origin code %d", errCode, code)
 	}
 	return errors.New(string(buf[:]))
 }
@@ -35,11 +31,7 @@ func Srv_ErrorText(code int) error {
 	var buf [length]byte
 	var errCode = C.Srv_ErrorText(C.int(code), (*C.char)(unsafe.Pointer(&buf[0])), length)
 	if errCode != 0 {
-		if e, ok := serverErrorsTable[int(errCode)]; ok {
-			return e
-		} else {
-			return fmt.Errorf("unknown error code %d", errCode)
-		}
+		return fmt.Errorf(" C.Srv_ErrorText failed code :%d,origin code %d", errCode, code)
 	}
 	return errors.New(string(buf[:]))
 }
