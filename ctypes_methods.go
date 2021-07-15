535951fa3add6e452d87e196e199b587a747763d
溪桥 <756445638@qq.com>
2021-07-15 10:22:45 +0800

    get cpu info methods

--- ctypes_methods.go
+++ ctypes_methods.go
@@ -1,17 +1,25 @@
 package snap7go
 
+func convertInt8SliceToString(s []int8) string {
+	ss := make([]byte, len(s))
+	for k, v := range s {
+		ss[k] = byte(v)
+	}
+	return string(ss)
+}
+
 func (t TS7CpuInfo) GetModuleTypeName() string {
-	panic("implemete me")
+	return convertInt8SliceToString(t.ModuleTypeName[:])
 }
 func (t TS7CpuInfo) GetSerialNumber() string {
-	panic("implemete me")
+	return convertInt8SliceToString(t.SerialNumber[:])
 }
 func (t TS7CpuInfo) GetASName() string {
-	panic("implemete me")
+	return convertInt8SliceToString(t.ASName[:])
 }
 func (t TS7CpuInfo) GetCopyright() string {
-	panic("implemete me")
+	return convertInt8SliceToString(t.Copyright[:])
 }
 func (t TS7CpuInfo) GetModuleName() string {
-	panic("implemete me")
+	return convertInt8SliceToString(t.ModuleName[:])
 }
